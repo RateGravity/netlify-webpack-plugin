a3f40744fcbd3c3c08ad6138068cf8375a828487
Christopher Pardy <chris@rategravity.com>
2020-04-03 14:05:37 -0400

    Support all Redirect Features
    Support all the Redirect Features

--- src/__tests__/redirects.test.ts
+++ src/__tests__/redirects.test.ts
@@ -52,4 +52,79 @@ describe("createRedirectFile", () => {
       "/netlify/*    https://www.netlify.com/docs/:splat    301"
     );
   });
+  it("outputs query matchers", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/bar-ma/:id",
+        query: {
+          state: "MA",
+          id: ":id"
+        }
+      }
+    ]);
+    expect(result).toBe(`/foo    state=MA    id=:id    /bar-ma/:id    301`);
+  });
+  it("outputs language conditions", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/foo.php",
+        conditions: {
+          language: ["en-us", "en-au"]
+        }
+      }
+    ]);
+    expect(result).toBe(`/foo    /foo.php    301    Language=en-us,en-au`);
+  });
+  it("outputs country conditions", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/foo.php",
+        conditions: {
+          country: ["US", "CA"]
+        }
+      }
+    ]);
+    expect(result).toBe("/foo    /foo.php    301    Country=US,CA");
+  });
+  it("outputs role conditions", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/foo.php",
+        conditions: {
+          role: ["admin", "user"]
+        }
+      }
+    ]);
+    expect(result).toBe("/foo    /foo.php    301    Role=admin,user");
+  });
+  it("outputs multiple conditions", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/mercia.html",
+        conditions: {
+          language: ["en-us"],
+          country: ["US"],
+          role: ["patriot"]
+        }
+      }
+    ]);
+    expect(result).toBe(
+      "/foo    /mercia.html    301    Language=en-us    Country=US    Role=patriot"
+    );
+  });
+  it("outputes no conditions", () => {
+    const result = createRedirectFile([
+      {
+        from: "/foo",
+        to: "/foo.php",
+        conditions: {}
+      }
+    ]);
+    expect(result).toBe("/foo    /foo.php    301");
+  });
 });
--- src/redirects.ts
+++ src/redirects.ts
@@ -1,14 +1,51 @@
+import { ISO3166 } from "./iso-3166";
+import { LanguageCodes } from "./language-codes";
+
+export interface Conditions {
+  readonly language?: LanguageCodes[];
+  readonly country?: ISO3166[];
+  readonly role?: string[];
+}
+
 export interface Redirect {
-  readonly 'from': string;
+  readonly from: string;
   readonly to: string;
   readonly status?: number;
   readonly force?: Boolean;
+  readonly query?: Record<string, string>;
+  readonly conditions?: Conditions;
+}
+
+function writeConditions(conditions?: Conditions): string[] {
+  if (conditions) {
+    return Object.keys(conditions).map(k => {
+      const key = k as keyof Conditions;
+      if (conditions[key] && conditions[key]!.length > 0) {
+        const capitalized = `${key[0].toUpperCase()}${key.substr(1)}`;
+        return `${capitalized}=${conditions[key]!.join(',')}`;
+      }
+      return null;
+    }).filter((v): v is string => v != null);
+  }
+  return [];
+}
+
+function writeQueryParams(query?: Record<string, string>): string[] {
+  if (query) {
+    return Object.keys(query).map(param => `${param}=${query[param]}`);
+  }
+  return [];
 }
 
 export const createRedirectFile = (patterns: Redirect[]): string =>
   patterns
-    .map(
-      ({ from, to, status = 301, force }) =>
-        `${from}    ${to}    ${status}${force ? "!" : ""}`
+    .map(({ from, query, to, status = 301, force, conditions }) =>
+      [
+        from,
+        ...writeQueryParams(query),
+        to,
+        `${status}${force ? "!" : ""}`,
+        ...writeConditions(conditions)
+      ].join("    ")
     )
     .join("\n");
